037703e8d30d1cb93781ea3322d6eaf9d98e0e82
alexandre <alexandrec658@gmail.com>
2025-02-06 14:40:24 -0300

    fix: test which was not passing with soma function

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(3, 6)
+	total := soma(3, 6)
 	if total != 9 {
 		t.Errorf("Resultado da soma é inválido: Resultado esperado: %d, Resultado obtido: %d", 9, total)
 	}
