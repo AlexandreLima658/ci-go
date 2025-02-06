cc14ced0bcdd7acdf1f256c0c8fee86f13112f9b
alexandre <alexandrec658@gmail.com>
2025-02-06 14:36:27 -0300

    feature: Unpublish soma function

--- math.go
+++ math.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	result := Soma(2,4);
+	result := soma(2,4);
 	fmt.Println(result)
 }
 
-func Soma(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
